037f2aee307abd402f2c9387af852e5f5800d280
Seonbeom Kim <kymj1@naver.com>
2019-01-03 18:42:22 +0900

    Add files via upload

--- biLM.py
+++ biLM.py
@@ -17,54 +17,156 @@ class biLM:
 		self.pad_idx = pad_idx # 0
 		self.window_size = window_size # for charCNN
 		self.filters = filters # for charCNN  np.sum(filters) = 2048
+		self.initializer = tf.initializers.random_uniform(-0.05, 0.05)
+
+		# lstm_char_cnn 짯던 코드 이용하자.
+		# cnn, highway, projection는 파라미터 공유, lstm은 공유하지않음. 
+		# fw, bw 별로 softmax_cross_entropy_with_logits 구하고 두 loss의 평균을 minimize
+
+		# data: [N*time_depth, word_length]
+		'''
+		input: s a b c /s    원래 word_length: 3,  token_Word_length: 5(s, /s 포함)
+			
+			forward_target: a b c /s
+			forward_input:  s a b c
+
+			backward_target: c b a s
+			backward_input: /s c b a
+		'''
+
 
 		with tf.name_scope("placeholder"):
-			if embedding_mode is 'char':
-				self.data = tf.placeholder(tf.int32, [None, None, None], name="char_x") # [N, word, char]
-			elif embedding_mode is 'word':
-				self.data = tf.placeholder(tf.int32, [None, None], name="word_x") # [N, word]
+			'''
+				input: s a b c /s    원래 word_length: 3,  token_Word_length: 5(s, /s 포함)
+				
+				forward_target: a b c /s
+				forward_input:  s a b c
 
-			self.target = tf.placeholder(tf.float32, [None, self.target_size], name="target") 
+				backward_target: c b a s
+				backward_input: /s c b a
+			'''
+
+
+			# data: [N*time_depth, word_length], 이 형식으로 넣어줘야 나중에 char embedding만 추출 가능(data: [N, word_length])
+			self.data = tf.placeholder(tf.int32, [None, self.word_length], name="char_x") # char_idx   form: s a b c /s 
+			self.target = tf.placeholder(tf.int32, [None, None], name="target") # word_idx  form: s a b c /s
 			self.lr = tf.placeholder(tf.float32, name="lr") # lr
+			self.keep_prob = tf.placeholder(tf.float32, name="keep_prob") # keep_prob
+			self.sentence_length = tf.placeholder(tf.int32, name='sentence_length')
+
+		with tf.name_scope('split_fw_bw'):
+			reshape_data = tf.reshape(self.data, [-1, self.sentence_length, self.word_length]) # [N, self.sentence_length, self.word_length]
+			fw_data = reshape_data[:, :-1, :] # s a b c /s => s a b c
+			fw_data = tf.reshape(fw_data, [-1, self.word_length]) # [N*self.sentence_length-1, self.word_length]
+			fw_target = self.target[:, 1:] # s a b c /s => a b c /s ,  [N, self.sentence_length-1]
+
+			bw_data = reshape_data[:, 1:, :] # s a b c /s => a b c /s
+			bw_data = tf.reverse(bw_data, axis=[1]) # a b c /s => /s c b a
+			bw_data = tf.reshape(bw_data, [-1, self.word_length]) # [N*self.sentence_length-1, self.word_length]
+			bw_target = self.target[:, :-1] # s a b c /s => s a b c
+			bw_target = tf.reverse(bw_target, axis=[1]) # s a b c => c b a s ,  [N, self.sentence_length-1]
 
 
-		with tf.name_scope("embedding"):		
-			if self.word_embedding is None:
-				self.embedding_table = self.make_embadding_table(pad_idx=self.pad_idx)
-				self.word_embedding = self.embedding_func(mode=embedding_mode)
+		with tf.name_scope("biLM"):
+			self.embedding_table = self.make_embadding_table(pad_idx=self.pad_idx)
 
-			#self.biLM_embedding = self.biLM(self.word_embedding, stack=self.stack) # [N, self.time_depth, self.embedding_size] * (self.stack+1)
-			self.fw_embedding, self.bw_embedding = self.biLM(self.word_embedding, stack=self.stack) # [N, self.time_depth, self.embedding_size] * (self.stack+1)
-			
-			# biLM 학습시에는 top layer만.
-			#self.biLM_embedding = self.biLM_embedding[-1] # [N, self.time_depth, self.embedding_size]
-			
-			# 학습된 biLM을 task에 적용할 때 사용.
-			#self.elmo_embedding = self._ELMo(self.biLM_embedding) # [N, self.time_depth, self.embeding_size]
+			# charCNN
+			fw_char_embedding = self.charCNN(
+					data=fw_data, 
+					window_size=self.window_size, 
+					filters=self.filters,
+					name='fw_bw_share'
+				) # [N*self.sentence_length-1, sum(filters)]
+			bw_char_embedding = self.charCNN(
+					data=bw_data, 
+					window_size=self.window_size, 
+					filters=self.filters,
+					name='fw_bw_share'
+				) # [N*self.sentence_length-1, sum(filters)]
+
+			# highway layer
+			for i in range(self.highway_stack):
+				fw_char_embedding = self.highway_network(
+						embedding=fw_char_embedding, 
+						units=np.sum(filters),
+						name=str(i)
+					) # [N*self.sentence_length-1, sum(filters)]
+				bw_char_embedding = self.highway_network(
+						embedding=bw_char_embedding, 
+						units=np.sum(filters),
+						name=str(i)
+					) # [N*self.sentence_length-1, sum(filters)]
+
+			# projection layer
+			fw_char_embedding = tf.layers.dense(
+					fw_char_embedding, 
+					units=self.embedding_size, 
+					activation=None, 
+					name='projection'
+				) # [N*self.sentence_length-1, self.embedding_size]
+			bw_char_embedding = tf.layers.dense(
+					bw_char_embedding, 
+					units=self.embedding_size, 
+					activation=None, 
+					name='projection', 
+					reuse=True # reuse same name weight
+				) # [N*self.sentence_length-1, self.embedding_size] 
+
+			# LSTM layer
+			self.fw_char_embedding = self.LSTM(
+					tf.reshape(fw_char_embedding, [-1, self.sentence_length-1, self.embedding_size]), 
+					stack=self.stack, 
+					name='fw'
+				) # [N, self.sentence_length-1, self.embedding_size] * (self.stack+1)
+			self.bw_char_embedding = self.LSTM(
+					tf.reshape(bw_char_embedding, [-1, self.sentence_length-1, self.embedding_size]),
+					bw_char_embedding, 
+					stack=self.stack, 
+					name='bw'
+				) # [N, self.sentence_length-1, self.embedding_size] * (self.stack+1)
+
 
 		with tf.name_scope('prediction'):
-			self.fw_pred = tf.layers.dense(self.fw_embedding[-1], units=self.target_size, activation=None, name='softmax_layer')
-			self.bw_pred = tf.layers.dense(self.bw_embedding[-1], units=self.target_size, activation=None, name='softmax_layer', reuse=True) #reuse same name weight
+			fw_pred = tf.layers.dense(
+					self.fw_char_embedding[-1], 
+					units=self.target_size, 
+					activation=None, 
+					name='softmax_layer'
+				) # [N, self.sentence_length-1, self.target_size] 
+			bw_pred = tf.layers.dense(
+					self.bw_char_embedding[-1], 
+					units=self.target_size, 
+					activation=None, 
+					name='softmax_layer', 
+					reuse=True # reuse same name weight
+				) # [N, self.sentence_length-1, self.target_size] 
 
 
 		with tf.name_scope('train'): 
-			target_one_hot = tf.one_hot(
-						self.target, # [None, self.target_length]
-						depth=self.target_size,
-						on_value = 1., # tf.float32
-						off_value = 0., # tf.float32
-					) # [N, self.target_length, self.voca_size]
-
+			fw_target_one_hot = tf.one_hot(
+					fw_target, # [N, self.sentence_length-1]
+					depth=self.target_size,
+					on_value = 1., # tf.float32
+					off_value = 0., # tf.float32
+				) # [N, self.sentence_length-1, self.target_size] 
+			bw_target_one_hot = tf.one_hot(
+					bw_target, # [N, self.sentence_length-1]
+					depth=self.target_size,
+					on_value = 1., # tf.float32
+					off_value = 0., # tf.float32
+				) # [N, self.sentence_length-1, self.target_size] 
+		
 			# calc train_cost
-			self.train_cost = tf.reduce_mean(
-						tf.nn.softmax_cross_entropy_with_logits(labels=target_one_hot, logits=self.pred)
-					) # softmax_cross_entropy_with_logits: # [N, self.target_length] => reduce_mean: scalar
+			fw_cost = tf.reduce_mean(
+					tf.nn.softmax_cross_entropy_with_logits(labels=fw_target_one_hot, logits=fw_pred)
+				) # softmax_cross_entropy_with_logits: [N, self.sentence_length-1] => reduce_mean: scalar
+			bw_cost = tf.reduce_mean(
+					tf.nn.softmax_cross_entropy_with_logits(labels=bw_target_one_hot, logits=bw_pred)
+				) # softmax_cross_entropy_with_logits: [N, self.sentence_length-1] => reduce_mean: scalar
+			self.cost = (fw_cost + bw_cost)/2 # fw bw mean cost
+
 			optimizer = tf.train.AdamOptimizer(self.lr)
-			self.minimize = optimizer.minimize(self.train_cost)
-
-
-		with tf.name_scope('metric'):
-			self.correct_check = tf.reduce_sum(tf.cast( tf.equal( tf.argmax(self.pred, 1), tf.argmax(self.target, 1) ), tf.int32 ))
+			self.minimize = optimizer.minimize(self.cost)
 
 
 		with tf.name_scope("saver"):
@@ -82,130 +184,113 @@ class biLM:
 
 	def make_embadding_table(self, pad_idx):
 		zero = tf.zeros([1, self.embedding_size], dtype=tf.float32) # for padding
-		embedding_table = tf.Variable(tf.random_normal([self.voca_size-1, self.embedding_size])) 
+		embedding_table = tf.get_variable(
+				name='embedding_table', 
+				shape=[self.voca_size-1, self.embedding_size],
+				initializer=self.initializer
+			)
 		front, end = tf.split(embedding_table, [pad_idx, -1])
 		embedding_table = tf.concat((front, zero, end), axis=0)
 		return embedding_table
 
 
 	def convolution(self, embedding, embedding_size, window_size, filters):
+		# embedding: [N*time_depth, word_length, self.embedding_size, 1]
 		convolved_features = []
 		for i in range(len(window_size)):
 			convolved = tf.layers.conv2d(
-						inputs = embedding, 
-						filters = filters[i], 
-						kernel_size = [window_size[i], embedding_size], 
-						strides=[1, 1], 
-						padding='VALID', 
-						activation=tf.nn.tanh
-					) # [N, ?, 1, filters]
-			convolved_features.append(convolved) # [N, ?, 1, filters] 이 len(window_size) 만큼 존재.
+					inputs = embedding, 
+					filters = filters[i], 
+					kernel_size = [window_size[i], embedding_size], 
+					strides=[1, 1], 
+					padding='VALID', 
+					activation=tf.nn.tanh,
+					kernel_initializer=self.initializer,
+					bias_initializer=self.initializer
+				) # [N, ?, 1, filters]
+			convolved_features.append(convolved) # [N*time_depth, ?, 1, filters] * len(window_size).
 		return convolved_features
 
 
 	def max_pooling(self, convolved_features):
+		# convolved_features: [N*time_depth, ?, 1, filters] * len(window_size)
 		pooled_features = []
-		for convolved in convolved_features: # [N, ?, 1, self.filters]
+		for convolved in convolved_features: # [N*time_depth, ?, 1, self.filters]
 			max_pool = tf.reduce_max(
-						input_tensor = convolved,
-						axis = 1,
-						keep_dims = True
-					) # [N, 1, 1, self.filters]
-			pooled_features.append(max_pool) # [N, 1, 1, self.filters] 이 len(window_size) 만큼 존재.
+					input_tensor = convolved,
+					axis = 1,
+					keep_dims = True
+				) # [N, 1, 1, self.filters]
+			max_pool = tf.layers.flatten(max_pool) # [N*time_depth, self.filters[i]]
+			pooled_features.append(max_pool) # [N*time_depth, self.filters[i]] * len(window_size).
 		return pooled_features
 
 
-	def charCNN(self, window_size, filters):
-		len_word = tf.shape(self.data)[1] # word length
+	def charCNN(self, data, window_size, filters, name):
+		with tf.variable_scope('charCNN_'+name, reuse=tf.AUTO_REUSE):
+			embedding = tf.nn.embedding_lookup(self.embedding_table, self.data) # [N*time_depth, word_length, self.embedding_size] 
+			embedding = tf.expand_dims(embedding, axis=-1) # [N*time_depth, word_length, self.embedding_size, 1]  => convolution을 위해 channel 추가.
 
-		embedding = tf.nn.embedding_lookup(self.embedding_table, self.data) # [N, word, char, self.embedding_size] 
-		embedding = tf.reshape(embedding, [-1, tf.shape(embedding)[2], self.embedding_size]) # [N*word, char, self.embedding_size]
-			# => convolution 적용하기 위해서 word는 batch화 시킴. 동일하게 적용되도록.
-		embedding = tf.expand_dims(embedding, axis=-1) # [N*word, char, self.embedding, 1]
-			# => convolution을 위해 channel 추가.
+			convolved_embedding = self.convolution(embedding, self.embedding_size, window_size, filters) # [N*time_depth, ?, 1, filters] * len(window_size).
+			max_pooled_embedding = self.max_pooling(convolved_features=convolved_embedding) # [N*time_depth, self.filters[i]] * len(window_size).
 
-		convolved_embedding = self.convolution(embedding, self.embedding_size, window_size, filters)
-			# => [N*word, ?, 1, filters] 이 len(window_size) 만큼 존재.
-		max_pooled_embedding = self.max_pooling(convolved_features=convolved_embedding)
-			# => [N*word, 1, 1, filters] 이 len(window_size) 만큼 존재. 
-		embedding = tf.concat(max_pooled_embedding, axis=-1) # [N*word, 1, 1, sum(filters)]
-			# => filter 기준으로 concat
-		embedding = tf.reshape(embedding, [-1, len_word, np.sum(filters)]) # [N, word, sum(filters)]
-		return embedding		
-
-
-	def highway_network(self, embedding):
-		# embedding: [N, word, sum(filters)]
-		transform_gate = tf.layers.dense(embedding, units=np.sum(self.filters), activation=tf.nn.sigmoid) # [N, word, sum(filters)]
-		carry_gate = 1-transform_gate # [N, word, sum(*filters)]
-		block_state = tf.layers.dense(embedding, units=np.sum(self.filters), activation=tf.nn.relu)
-		highway = transform_gate * block_state + carry_gate * embedding # [N, word, sum(filters)]
-			# if transfor_gate is 1. then carry_gate is 0. so only use block_state
-			# if transfor_gate is 0. then carry_gate is 1. so only use embedding
-			# if transfor_gate is 0.@@. then carry_gate is 0.@@. so use sum of scaled block_state and embedding
-		return highway
-
-
-	def embedding_func(self, mode=None):
-		if mode is 'word':
-			embedding = tf.nn.embedding_lookup(self.embedding_table, self.data) # [N, word, self.embedding_size]
-			return embedding
-
-		elif mode is 'char':
-			# charCNN
-			embedding = self.charCNN(window_size=self.window_size, filters=self.filters) # [N, word, filters*len(window_size)]
-			# two highway layer
-			embedding = self.highway_network(embedding=embedding) # [N, word, filters*len(window_size)]
-			embedding = self.highway_network(embedding=embedding) # [N, word, filters*len(window_size)]
-			# linear projection
-			embedding = tf.layers.dense(embedding, units=self.embedding_size, activation=None)
+			embedding = tf.concat(max_pooled_embedding, axis=-1) # [N*time_depth, sum(filters)], filter 기준으로 concat
 			return embedding
 
 
-		"""
-		data = [[[3, 3, 3, 2, 3, 1, 3, 1, 3], [0, 3, 1, 2, 2, 0, 3, 2, 1], [3, 3, 3, 2, 3, 1, 3, 1, 3]],
-	 		[[3, 3, 3, 2, 2, 2, 2, 3, 1], [0, 0, 2, 2, 2, 0, 3, 3, 0], [3, 3, 3, 2, 3, 1, 3, 1, 3]]]
-	 	data를 self.data에 feed해서 self.charCNN 돌려보면 같은 char로 이뤄진 단어는 같은 embedding을 갖는것을 확인할 수 있음.
-		"""
 
-	def biLM(self, data, stack):
-		# data [N, self.time_depth, self.embedding_size(512)]
+	def highway_network(self, embedding, units, name):
+		with tf.variable_scope('highway_'+name, reuse=tf.AUTO_REUSE):
+			# embedding: [N*time_depth, sum(filters)]
+			transform_gate = tf.layers.dense(
+					embedding, 
+					units=units, 
+					activation=tf.nn.sigmoid, 
+					kernel_initializer=self.initializer,
+					bias_initializer=tf.constant_initializer(-2)
+				) # [N*time_depth, sum(filters)]
+			carry_gate = 1-transform_gate # [N*time_depth, sum(filters)]
+			block_state = tf.layers.dense(
+					embedding, 
+					units=units, 
+					activation=tf.nn.relu,
+					kernel_initializer=self.initializer,
+					bias_initializer=self.initializer
+				) # [N*time_depth, sum(filters)]
+			highway = transform_gate * block_state + carry_gate * embedding # [N*time_depth, sum(filters)]
+				# if transfor_gate is 1. then carry_gate is 0. so only use block_state
+				# if transfor_gate is 0. then carry_gate is 1. so only use embedding
+				# if transfor_gate is 0.@@. then carry_gate is 0.@@. so use sum of scaled block_state and embedding
+			return highway # [N*time_depth, sum(filters)]
+
+
+
+	def LSTM(self, data, stack, name):
+		# data: [N, self.sentence_length-1, self.embedding_size]
 		# stack:2 
 		# cell_num: 4096
 		# hiddenlayer를 512차원으로 줄여줄 projection 적용하고, residual connection 연결한다.
  			# 이 512차원이 입력단어의 벡터가 됨.
  		# 양방향은 파라미터 전부 공유(softmax하는 layer도 포함.)
 
-		with tf.variable_scope('biLM') as scope:
-			fw_input = data
-			bw_input = tf.reverse(data, axis=[1])
-		
-			fw_layer_val = [fw_input]
-			bw_layer_val = [bw_input]
+		with tf.variable_scope('lstm_'+name) as scope:
+			layer_val = [data]
 
 			for i in range(stack):
-
-				# https://www.tensorflow.org/api_docs/python/tf/contrib/rnn/LayerNormBasicLSTMCell
-				cell_fw = tf.contrib.rnn.LSTMCell(self.cell_num)
-				cell_bw = tf.contrib.rnn.LSTMCell(self.cell_num)
+				cell = tf.contrib.rnn.LSTMCell(self.cell_num)
 				
-				# fw_bw_val: shape: [N, self.time_depth, self.cell_num]
-				fw_val, _ = tf.nn.dynamic_rnn(cell_fw, fw_input, dtype=tf.float32, scope='stack_fw'+str(i))				
-				bw_val, _ = tf.nn.dynamic_rnn(cell_bw, bw_input, dtype=tf.float32, scope='stack_bw'+str(i))
+				# data: shape: [N, self.sentence_length-1, self.cell_num]
+				data, _ = tf.nn.dynamic_rnn(cell, data, dtype=tf.float32, scope='stack_'+name+'_'+str(i))				
 
-				# linear projection, shape: [N, self.time_depth, self.embedding_size]
-				linear_fw_val = tf.layers.dense(fw_val, units=self.embedding_size, activation=None, name='linear_fw'+str(i))
-				linear_bw_val = tf.layers.dense(bw_val, units=self.embedding_size, activation=None, name='linear_bw'+str(i))#, reuse=True) #reuse same name weight
+				# linear projection, shape: [N, self.sentence_length-1, self.embedding_Size]
+				linear_data = tf.layers.dense(data, units=self.embedding_size, activation=None, name='linear_'+name+'_'+str(i))
 			
 				# save current layer state for residual connection and ELMo
-				fw_layer_val.append(linear_fw_val)
-				bw_layer_val.append(linear_bw_val)
+				layer_val.append(linear_data)
 
 				# update next layer input
 				if i < stack-1:
-					# update next layer input
-					fw_input = linear_fw_val + fw_input
-					bw_input = linear_bw_val + bw_input
+					data = linear_data + data
 					
-			return fw_layer_val, bw_layer_val
+			return layer_val
 				
